e533f2f29a824b4518eed97915c1dad9c63de092
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 23:43:35 +0200

    compilation fixed

--- src/org/intellij/plugins/ceylon/structureView/CeylonStructureViewFactory.java
+++ src/org/intellij/plugins/ceylon/structureView/CeylonStructureViewFactory.java
@@ -1,29 +1,34 @@
 package org.intellij.plugins.ceylon.structureView;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class CeylonStructureViewFactory implements PsiStructureViewFactory {
+public class CeylonStructureViewFactory implements PsiStructureViewFactory
+{
+	@Nullable
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@NotNull
+			@Override
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new CeylonFileTreeModel(psiFile);
+			}
 
-    @Nullable
-    @Override
-    public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-        return new TreeBasedStructureViewBuilder() {
-            @NotNull
-            @Override
-            public StructureViewModel createStructureViewModel() {
-                return new CeylonFileTreeModel(psiFile);
-            }
-
-            @Override
-            public boolean isRootNodeShown() {
-                return false;
-            }
-        };
-    }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
