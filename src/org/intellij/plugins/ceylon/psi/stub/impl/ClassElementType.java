42bdde28a3f81f4c46f0b462b1ff72e829254b84
VISTALL <vistall.valeriy@gmail.com>
2014-06-19 12:44:31 +0400

    #COP-125 fixed

--- src/org/intellij/plugins/ceylon/psi/stub/impl/ClassElementType.java
+++ src/org/intellij/plugins/ceylon/psi/stub/impl/ClassElementType.java
@@ -1,6 +1,7 @@
 package org.intellij.plugins.ceylon.psi.stub.impl;
 
-import com.intellij.psi.stubs.*;
+import java.io.IOException;
+
 import org.intellij.plugins.ceylon.CeylonLanguage;
 import org.intellij.plugins.ceylon.psi.CeylonClass;
 import org.intellij.plugins.ceylon.psi.impl.CeylonClassDeclarationImpl;
@@ -9,67 +10,88 @@ import org.intellij.plugins.ceylon.psi.stub.ClassIndex;
 import org.intellij.plugins.ceylon.psi.stub.ClassStub;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.stubs.IStubElementType;
+import com.intellij.psi.stubs.IndexSink;
+import com.intellij.psi.stubs.StubElement;
+import com.intellij.psi.stubs.StubInputStream;
+import com.intellij.psi.stubs.StubOutputStream;
 
-import java.io.IOException;
+public class ClassElementType extends IStubElementType<ClassStub, CeylonClass>
+{
 
-public class ClassElementType extends IStubElementType<ClassStub, CeylonClass> {
+	public ClassElementType(@NotNull @NonNls String debugName)
+	{
+		super(debugName, CeylonLanguage.INSTANCE);
+	}
 
-    public ClassElementType(@NotNull @NonNls String debugName) {
-        super(debugName, CeylonLanguage.INSTANCE);
-    }
+	@Override
+	public CeylonClass createPsi(@NotNull ClassStub stub)
+	{
+		if(stub.isInterface())
+		{
+			return new CeylonInterfaceDeclarationImpl(stub, this);
+		}
+		return new CeylonClassDeclarationImpl(stub, this);
+	}
 
-    @Override
-    public CeylonClass createPsi(@NotNull ClassStub stub) {
-        if (stub.isInterface()) {
-            return new CeylonInterfaceDeclarationImpl(stub, this);
-        }
-        return new CeylonClassDeclarationImpl(stub, this);
-    }
+	@Override
+	public ClassStub createStub(@NotNull CeylonClass psi, StubElement parentStub)
+	{
+		byte flags = 0;
 
-    @Override
-    public ClassStub createStub(@NotNull CeylonClass psi, StubElement parentStub) {
-        byte flags = 0;
+		if(psi.isInterface())
+		{
+			flags |= ClassStubImpl.INTERFACE;
+		}
+		else if(psi.isObject())
+		{
+			flags |= ClassStubImpl.OBJECT;
+		}
 
-        if (psi.isInterface()) {
-            flags |= ClassStubImpl.INTERFACE;
-        } else if (psi.isObject()) {
-            flags |= ClassStubImpl.OBJECT;
-        }
+		return new ClassStubImpl((IStubElementType) psi.getNode().getElementType(), parentStub, psi.getName(), psi.getQualifiedName(), flags);
+	}
 
-        return new ClassStubImpl((IStubElementType) psi.getNode().getElementType(), parentStub, psi.getName(), psi.getQualifiedName(), flags);
-    }
+	@NotNull
+	@Override
+	public String getExternalId()
+	{
+		return "ceylon." + toString();
+	}
 
-    @Override
-    public String getExternalId() {
-        return "ceylon.class";
-    }
+	@Override
+	public void serialize(@NotNull ClassStub stub, @NotNull StubOutputStream dataStream) throws IOException
+	{
+		dataStream.writeName(stub.getName());
+		dataStream.writeName(stub.getQualifiedName());
 
-    @Override
-    public void serialize(ClassStub stub, StubOutputStream dataStream) throws IOException {
-        dataStream.writeName(stub.getName());
-        dataStream.writeName(stub.getQualifiedName());
+		byte flags = 0;
+		if(stub.isInterface())
+		{
+			flags |= ClassStubImpl.INTERFACE;
+		}
+		else if(stub.isObject())
+		{
+			flags |= ClassStubImpl.OBJECT;
+		}
+		dataStream.writeByte(flags);
+	}
 
-        byte flags = 0;
-        if (stub.isInterface()) {
-            flags |= ClassStubImpl.INTERFACE;
-        } else if (stub.isObject()) {
-            flags |= ClassStubImpl.OBJECT;
-        }
-        dataStream.writeByte(flags);
-    }
+	@NotNull
+	@Override
+	public ClassStub deserialize(@NotNull StubInputStream dataStream, StubElement parentStub) throws IOException
+	{
+		return new ClassStubImpl(parentStub, dataStream.readName(), dataStream.readName(), dataStream.readByte());
+	}
 
-    @Override
-    public ClassStub deserialize(StubInputStream dataStream, StubElement parentStub) throws IOException {
-        return new ClassStubImpl(parentStub, dataStream.readName(), dataStream.readName(), dataStream.readByte());
-    }
+	@Override
+	public void indexStub(@NotNull ClassStub stub, @NotNull IndexSink sink)
+	{
+		String name = stub.getQualifiedName();
 
-    @Override
-    public void indexStub(ClassStub stub, IndexSink sink) {
-        String name = stub.getQualifiedName();
-
-        if (name != null) {
-            sink.occurrence(ClassIndex.KEY, name);
-        }
-        // TODO short name index
-    }
+		if(name != null)
+		{
+			sink.occurrence(ClassIndex.KEY, name);
+		}
+		// TODO short name index
+	}
 }
